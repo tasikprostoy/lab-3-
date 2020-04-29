cd9631674a0b50f4bc8a8a952b5d9492b7e9b08a
tasikprostoy <tarasopaopa@gmail.com>
2020-04-29 16:58:04 +0300

    add tests

--- RESTfulExample/src/main/java/manager/DeviceManager.java
+++ RESTfulExample/src/main/java/manager/DeviceManager.java
@@ -2,13 +2,32 @@ package manager;
 
 import java.util.List;
 
+import enums.DeviceType;
+import enums.SortingType;
+
 import model.Device;
 
 public class DeviceManager {
 	private  List<Device> devices;
+	
+    public DeviceManager() {
+    	
+    }
     
 	public DeviceManager(List<Device> devices) {
 		this.devices = devices;
+	}
+	
+	public List<Device> sortByProducer(SortingType sortingType) {
+		return null;
+	}
+	
+	public List<Device> sortByPower(SortingType sortingType) {
+		return null;
+	}
+	
+	public List<Device> filterByType(DeviceType deviceType) {
+		return null;
 	}
 
 	public List<Device> getDevices() {
---
+++ RESTfulExample/src/test/java/DeviceManagerTest.java
@@ -0,0 +1,30 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import org.junit.jupiter.api.Test;
+
+import enums.DeviceType;
+import enums.SortingType;
+import manager.DeviceManager;
+
+public class DeviceManagerTest {
+	private DeviceManager manager = new DeviceManager();
+	@Test
+	public void testSortByProducer() {
+		assertNull(manager.sortByProducer(SortingType.ASC));
+
+	}
+	
+	@Test
+	public void testSortByPower() {
+		assertNull(manager.sortByPower(SortingType.ASC));
+
+	}
+	
+	@Test
+	public void testFilterByType() {
+		assertNull(manager.filterByType(DeviceType.LED));
+
+	}
+	
+}
